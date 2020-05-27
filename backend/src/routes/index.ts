a40bc8298c3e5e51016e2029f4dda285fe232d85
Alessandro <vyseclown@gmail.com>
2020-05-27 00:12:12 -0300

    trabalhando com DTO

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -4,6 +4,8 @@ import appointmentsRouter from './appointments.routes';
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
-routes.get('/', (request, response) => response.json({ message: 'Hello World' }));
+routes.get('/', (request, response) =>
+  response.json({ message: 'Hello World' }),
+);
 
 export default routes;
