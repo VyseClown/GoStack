1c21df5d3b3dafd616aaa979d57c36134b0c9308
Alessandro <vyseclown@gmail.com>
2020-05-26 23:44:50 -0300

    modulo 2 de nodejs

---
+++ backend/src/routes/index.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import appointmentsRouter from './appointments.routes';
+
+const routes = Router();
+
+routes.use('/appointments', appointmentsRouter);
+routes.get('/', (request, response) => response.json({ message: 'Hello World' }));
+
+export default routes;
