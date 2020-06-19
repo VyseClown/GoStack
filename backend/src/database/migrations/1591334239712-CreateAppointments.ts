97966eaa661a64232527b0d11f1efa5a8a54aede
Alessandro <vyseclown@gmail.com>
2020-06-18 23:53:34 -0300

    Finishing the correct use of TypeORM

--- backend/src/database/migrations/1591334239712-CreateAppointments.ts
+++ backend/src/database/migrations/1591334239712-CreateAppointments.ts
@@ -11,6 +11,7 @@ export class CreateAppointments1591334239712 implements MigrationInterface {
             type:'varchar',
             isPrimary:true,
             generationStrategy:'uuid',
+            default:'uuid_generate_v4()',
           },
           {
             name:'provider',
