cde3c266a3569d7084fad7fb660c2c2e94b28276
Alessandro <vyseclown@gmail.com>
2020-06-24 13:57:56 -0300

    Adding authentication control

--- backend/src/services/CreateUserService.ts
+++ backend/src/services/CreateUserService.ts
@@ -1,35 +1,37 @@
-import {getRepository} from 'typeorm'
-import {hash} from 'bcryptjs'
-import User from '../models/User'
+import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+import User from '../models/User';
 
-interface Request{
-  name:string;
-  email:string;
-  password:string;
+interface Request {
+  name: string;
+  email: string;
+  password: string;
 }
 
-class CreateUserService{
-  public async execute({name,email,password}:Request):Promise<User>{
+class CreateUserService {
+  public async execute({ name, email, password }: Request): Promise<User> {
     const usersRepository = getRepository(User);
 
     const checkUserExists = await usersRepository.findOne({
-      where:{email},
-    })
+      where: { email },
+    });
 
-    if(checkUserExists){
+    if (checkUserExists) {
       throw new Error('Email address already used.');
     }
 
-    const hashedPassword = await hash(password,8);
+    const hashedPassword = await hash(password, 8);
 
     const user = usersRepository.create({
-      name,email,password:hashedPassword
-    })
+      name,
+      email,
+      password: hashedPassword,
+    });
 
-    await usersRepository.save(user)
+    await usersRepository.save(user);
 
     return user;
   }
 }
 
-export default CreateUserService
+export default CreateUserService;
