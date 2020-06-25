d2bbd70120e007ab906ff67b44d682974b8824d1
Alessandro <vyseclown@gmail.com>
2020-06-25 00:21:35 -0300

    finished upload of avatar and exception handling

--- backend/src/services/AuthenticateUserService.ts
+++ backend/src/services/AuthenticateUserService.ts
@@ -4,6 +4,8 @@ import { sign } from 'jsonwebtoken';
 import User from '../models/User';
 import authConfig from '../config/auth';
 
+import AppError from '../errors/AppError';
+
 interface Request {
   email: string;
   password: string;
@@ -21,13 +23,13 @@ class AuthenticateUserService {
     const user = await userRepository.findOne({ where: { email } });
 
     if (!user) {
-      throw new Error('Incorrect email/password combination.');
+      throw new AppError('Incorrect email/password combination.', 401);
     }
 
     const passwordMathed = await compare(password, user.password);
 
     if (!passwordMathed) {
-      throw new Error('Incorrect email/password combination.');
+      throw new AppError('Incorrect email/password combination.', 401);
     }
 
     const { secret, expiresIn } = authConfig.jwt;
--- backend/src/services/CreateAppointmentService.ts
+++ backend/src/services/CreateAppointmentService.ts
@@ -1,37 +1,36 @@
 import { startOfHour } from 'date-fns';
-import {getCustomRepository} from 'typeorm'
-import Appointment from './../models/Appointment';
-import AppointmentsRepository from './../repositories/AppointmentsRepository';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
-
-interface Request{
-  provider_id:string;
-  date:Date;
+interface Request {
+  provider_id: string;
+  date: Date;
 }
 
 class CreateAppointmentService {
-
-  public async execute({date,provider_id}:Request): Promise<Appointment>{
+  public async execute({ date, provider_id }: Request): Promise<Appointment> {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
     const appointmentDate = startOfHour(date);
 
-  const findAppointmentInSameDate = await appointmentsRepository.findByDate(
-    appointmentDate,
-  );
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+      appointmentDate,
+    );
 
-  if (findAppointmentInSameDate) {
-    throw Error('This appointment is already booked')
-  }
+    if (findAppointmentInSameDate) {
+      throw AppError('This appointment is already booked');
+    }
 
-  const appointment = appointmentsRepository.create({
-    provider_id,
-    date: appointmentDate,
-  });
+    const appointment = appointmentsRepository.create({
+      provider_id,
+      date: appointmentDate,
+    });
 
-  await appointmentsRepository.save(appointment)
+    await appointmentsRepository.save(appointment);
 
-  return appointment;
+    return appointment;
   }
 }
-export default CreateAppointmentService
+export default CreateAppointmentService;
